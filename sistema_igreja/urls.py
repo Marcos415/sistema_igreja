4464a3dd783bf9422d4d91708dcba8781ca9708b
marcos sousa <markinhosmms1991@gmail.com>
2025-08-01 16:59:37 -0400

    Redirect root URL to /gestao/

--- sistema_igreja/urls.py
+++ sistema_igreja/urls.py
@@ -1,23 +1,26 @@
-# sistema_igreja/urls.py
+"""
+URL configuration for sistema_igreja project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
-# Adicione esta linha para importar as views do seu app 'membros'
-# Certifique-se que 'membros' é o nome do seu aplicativo Django.
-# Se você tiver uma view diretamente no seu urls.py principal,
-# precisará importar ela ou o módulo de views correspondente.
-# Dado o erro, parece que você está tentando usar views do app 'membros' diretamente aqui.
-from membros import views # <--- ADICIONE ESTA LINHA!
+from django.views.generic.base import RedirectView # Importe RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Esta linha provavelmente deveria ser assim para incluir as URLs do seu app 'membros':
+    # Redireciona a URL raiz ('/') para '/gestao/'
+    path('', RedirectView.as_view(url='/gestao/', permanent=False)), # permanent=False para 302 temporário
     path('gestao/', include('membros.urls')), # O prefixo 'gestao/' para todas as suas URLs de membros
-    
-    # Se você tem algumas URLs diretas aqui, elas precisam da importação 'views'
-    # path('membros/listar/', views.listar_membros, name='listar_membros'), # Esta linha gerou o erro
-    # Recomendo que as URLs do seu app 'membros' fiquem no 'membros/urls.py'
-    # e sejam incluídas aqui com 'include('membros.urls')' como na linha acima.
-    # Se a linha acima (path('gestao/', include('membros.urls'))) já existe e está correta,
-    # você pode remover a linha problematica: path('membros/listar/', views.listar_membros, name='listar_membros'),
-    # pois ela já estaria sendo tratada dentro de 'membros.urls' via 'include'.
 ]
